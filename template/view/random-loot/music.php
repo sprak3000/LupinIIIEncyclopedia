3a692908cd4ab6e8bf0ea9ad9d30a941ca36a371
Luis Cruz <sprak3000+github@gmail.com>
2015-06-25 19:11:57 -0400

    Final music entries

--- template/view/random-loot/music.php
+++ template/view/random-loot/music.php
@@ -400,17 +400,346 @@
 
   </div>
 
+  <div class="row margin-bottom-30">
+
+    <div class="col-md-8">
+      <h2>Isn't It More Lupintic?</h2>
+      <ol>
+        <li>Theme From Lupin III (More Lupintic Version)</li>
+        <li>Angel Dance (More Lupintic Version)</li>
+        <li>Across The Wind</li>
+        <li>Lonesome City Breeze</li>
+        <li>Sweet On You</li>
+        <li>Saudades Do Rio (More Lupintic Version)</li>
+        <li>Isn't It More Lupintic?</li>
+        <li>Midnight Cruise (More Lupintic Version)</li>
+        <li>Cool Wave</li>
+        <li>Manhattan Serenade</li>
+        <li>Life's a Flame</li>
+      </ol>
+    </div>
+
+    <div class="col-md-4">
+      <div class="rounded-2x service-block-light">
+        <img class="img-responsive center-block" src="/dist/asset/img/music/isnt-it-more-lupintic.jpg" alt="Isn't It More Lupintic? CD cover" />
+        <dl class="dl-horizontal">
+          <dt>Company:</dt><dd>Vap</dd>
+          <dt>Catalog #:</dt><dd>VPCG-84703</dd>
+          <dt>Release Date:</dt><dd>2000 July 21</dd>
+          <dt>Price:</dt><dd>2500&yen;</dd>
+          <dt># of Discs:</dt><dd>1</dd>
+          <dt>Artist(s):</dt><dd>Ohno Yuji (all tracks)</dd>
+        </dl>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="row margin-bottom-30">
+    <div class="col-md-4">
+      <div class="rounded-2x service-block-light">
+        <img class="img-responsive center-block" src="/dist/asset/img/music/lupin-the-third-jazz-the-3rd-funky-pop.jpg" alt="Lupin the Third Jazz the 3rd (Funky &amp; Pop) CD cover" />
+        <dl class="dl-horizontal">
+          <dt>Company:</dt><dd>Vap</dd>
+          <dt>Catalog #:</dt><dd>VPCG-84728</dd>
+          <dt>Release Date:</dt><dd>2001 March 16</dd>
+          <dt>Price:</dt><dd>2500&yen;</dd>
+          <dt># of Discs:</dt><dd>1</dd>
+          <dt>Artist(s):</dt><dd>Ohno Yuji (all tracks)</dd>
+        </dl>
+      </div>
+    </div>
+
+    <div class="col-md-8">
+      <h2>Lupin the Third Jazz the 3rd (Funky &amp; Pop)</h2>
+      <ol>
+        <li>Theme from Lupin the 3rd (Funky &amp; Pop version)</li>
+        <li>Woman from Memphis</li>
+        <li>Treasures of Time (Funky &amp; Pop version)</li>
+        <li>Mayflower</li>
+        <li>Tell Me Baby</li>
+        <li>Zenigata Rock</li>
+        <li>The Third</li>
+        <li>Hey! Mr.Lucky</li>
+        <li>Memory of Smile</li>
+      </ol>
+    </div>
+
+  </div>
+
+  <div class="row margin-bottom-30">
+
+    <div class="col-md-8">
+      <h2>Lupin the Third Part III Ongakushu</h2>
+      <div class="row">
+        <div class="col-md-5">
+          <ol>
+            <li>Sexy Adventure</li>
+            <li>Prayer for Corsica</li>
+            <li>Midnight Orient Express</li>
+            <li>Harboring the Goddess of Freedom</li>
+            <li>Lupin vs. Scotland Yard</li>
+            <li>Strange Downtown</li>
+            <li>Tropical Sailing</li>
+            <li>Hopeless Situation</li>
+          </ol>
+        </div>
+        <div class="col-md-5">
+          <ol start="9">
+            <li>In the Crimson Shadow of Golden Gate</li>
+            <li>Secret Passage</li>
+            <li>Monte Carlo 3AM</li>
+            <li>Do-or-Die Spirit of Escape</li>
+            <li>The Andean Wind</li>
+            <li>Continental Crossing Freeway</li>
+            <li>Good Night Lupin Sexy Adventure</li>
+            <li>Fairy Night</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="rounded-2x service-block-light">
+        <img class="img-responsive center-block" src="/dist/asset/img/music/lupin-the-third-part-iii-ongakushu.jpg" alt="Lupin the Third Part III Ongakushu CD cover" />
+        <dl class="dl-horizontal">
+          <dt>Company:</dt><dd>Vap</dd>
+          <dt>Catalog #:</dt><dd>VPCG-84716</dd>
+          <dt>Release Date:</dt><dd>2001 June 21</dd>
+          <dt>Price:</dt><dd>1800&yen;</dd>
+          <dt># of Discs:</dt><dd>1</dd>
+          <dt>Artist(s):</dt><dd>Ohno Yuji (all tracks)</dd><dd>Sonia Rossa (track 16)</dd>
+        </dl>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="row margin-bottom-30">
+    <div class="col-md-4">
+      <div class="rounded-2x service-block-light">
+        <img class="img-responsive center-block" src="/dist/asset/img/music/lupin-the-third-rebirth-from-71-original-score.jpg" alt="Lupin the Third Rebirth from '71 Original Score CD cover" />
+        <dl class="dl-horizontal">
+          <dt>Company:</dt><dd>Vap</dd>
+          <dt>Catalog #:</dt><dd>VPCC-81406</dd>
+          <dt>Release Date:</dt><dd>2002 January 23</dd>
+          <dt>Price:</dt><dd>3000&yen;</dd>
+          <dt># of Discs:</dt><dd>1</dd>
+          <dt>Artist(s):</dt><dd>Takeo Yamashita (all tracks)</dd><dd>Charlie Kosei (2, 4, 12, 13)</dd><dd>Yoshirou Hiroishi (1, 6)</dd><dd>Takanashi Kayo (3, 5, 7, 8, 10, 11)</dd><dd>Megumi (14)</dd>
+        </dl>
+      </div>
+    </div>
+
+    <div class="col-md-8">
+      <h2>Lupin the Third Rebirth from '71 Original Score</h2>
+      <div class="row">
+        <div class="col-md-6">
+          <ol>
+            <li>Lupin the Third Theme Song 3</li>
+            <li>Afro Lupin '68</li>
+            <li>Scat Theme</li>
+            <li>Yeah! Lupin</li>
+            <li>Lupin the Third Theme Song 1 (Scat Version)</li>
+            <li>Lupin the Third Theme Song 3 (Shuffle Version)</li>
+            <li>Gallop Samba</li>
+          </ol>
+        </div>
+        <div class="col-md-6">
+          <ol start="8">
+            <li>Lupin the Third Theme Song 2 (Scat Version)</li>
+            <li>Rhythm Suspence</li>
+            <li>Lupin Walkin'</li>
+            <li>Nice Guy Lupin</li>
+            <li>Lupin the Third Theme Song 1</li>
+            <li>Lupin the Third Theme Song 2</li>
+            <li>Gupie de Lupin</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="row margin-bottom-30">
+
+    <div class="col-md-8">
+      <h2>Guy's Heart Charlie's Lupin Songs</h2>
+      <div class="row">
+        <div class="col-md-6">
+          <ol>
+            <li>Lupin the Third Theme Song 2 (Swing version)</li>
+            <li>Lupin the Third Theme Song 3</li>
+            <li>Afro Lupin '68</li>
+            <li>Lupin the Third Theme Song 1 (Bossa version)</li>
+            <li>Lupin the Third Theme Song 1 (Ballad version)</li>
+            <li>Scat Theme</li>
+          </ol>
+        </div>
+        <div class="col-md-6">
+          <ol start="7">
+            <li>Nice Guy Lupin</li>
+            <li>Lupin the Third Theme Song 2 (Ballad version)</li>
+            <li>Hey Mr. Dandy (new song by Charlie)</li>
+            <li>Sharp Work Stealer (new song by Charlie)</li>
+            <li>Blues of the Third (new song composed by Yamashita Takeo)</li>
+            <li>Lupin the Third Theme Song 2 (Charlie's version)</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="rounded-2x service-block-light">
+        <img class="img-responsive center-block" src="/dist/asset/img/music/guys-heart-charlies-lupin-songs.jpg" alt="Guy's Heart Charlie's Lupin Songs CD cover" />
+        <dl class="dl-horizontal">
+          <dt>Company:</dt><dd>Vap</dd>
+          <dt>Catalog #:</dt><dd>VPCG-81430</dd>
+          <dt>Release Date:</dt><dd>2002 May 22</dd>
+          <dt>Price:</dt><dd>2800&yen;</dd>
+          <dt># of Discs:</dt><dd>1</dd>
+          <dt>Artist(s):</dt><dd>Charlie Kosei (all tracks)</dd>
+        </dl>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="row margin-bottom-30">
+    <div class="col-md-4">
+      <div class="rounded-2x service-block-light">
+        <img class="img-responsive center-block" src="/dist/asset/img/music/lupin-the-third-jazz-another-jazz.jpg" alt="Lupin the Third Jazz Another &quot;Jazz&quot; CD cover" />
+        <dl class="dl-horizontal">
+          <dt>Company:</dt><dd>Vap</dd>
+          <dt>Catalog #:</dt><dd>VPCG-84765</dd>
+          <dt>Release Date:</dt><dd>2002 July 24</dd>
+          <dt>Price:</dt><dd>2500&yen;</dd>
+          <dt># of Discs:</dt><dd>1</dd>
+          <dt>Artist(s):</dt><dd>Ohno Yuji/You &amp; Explosion Band (all tracks)</dd>
+        </dl>
+      </div>
+    </div>
+
+    <div class="col-md-8">
+      <h2>Lupin the Third Jazz Another &quot;Jazz&quot;</h2>
+      <div class="row">
+        <div class="col-md-6">
+          <ol>
+            <li>Lupin the Third Theme '78 (2002 Version)</li>
+            <li>Funky Junky Trip</li>
+            <li>Isn't It Lupintic?</li>
+            <li>Going out on the Town</li>
+            <li>What's the Worry?</li>
+            <li>JFK Memories</li>
+            <li>Theme from Lupin III (More Lupintic Version)</li>
+          </ol>
+        </div>
+        <div class="col-md-5">
+          <ol start="8">
+            <li>A Day of the Town</li>
+            <li>Life's a Flame</li>
+            <li>Midnight Cruise (More Lupintic Version)</li>
+            <li>Angel Dance (More Lupintic Version)</li>
+            <li>Saudades Do Rio (More Lupintic Version)</li>
+            <li>Lupin the Third (Vocal)</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="row margin-bottom-30">
+
+    <div class="col-md-8">
+      <h2>Lupin the Third Jazz Bossa &amp; Fusion</h2>
+      <ol>
+        <li>Lupin the Third (A tarde cai)</li>
+        <li>Lonely for the Road</li>
+        <li>Magnum Dance</li>
+        <li>Stolen Moments (Flor espacial)</li>
+        <li>Love in Sao Paulo</li>
+        <li>Manhattan Joke</li>
+        <li>Just Fall in Love</li>
+        <li>Memory of Smile (Pequena luz)</li>
+        <li>Urban Shadow</li>
+      </ol>
+    </div>
+
+    <div class="col-md-4">
+      <div class="rounded-2x service-block-light">
+        <img class="img-responsive center-block" src="/dist/asset/img/music/lupin-the-third-jazz-bossa-fusion.jpg" alt="Lupin the Third Jazz Bossa &amp; Fusion CD cover" />
+        <dl class="dl-horizontal">
+          <dt>Company:</dt><dd>Vap</dd>
+          <dt>Catalog #:</dt><dd>VPCG-84764</dd>
+          <dt>Release Date:</dt><dd>2002 July 24</dd>
+          <dt>Price:</dt><dd>2500&yen;</dd>
+          <dt># of Discs:</dt><dd>1</dd>
+          <dt>Artist(s):</dt><dd>Ohno Yuji &amp; Friends (all tracks)</dd>
+        </dl>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="row margin-bottom-30">
+
+    <div class="col-md-4">
+      <div class="rounded-2x service-block-light">
+        <img class="img-responsive center-block" src="/dist/asset/img/music/lupin-the-third-jazz-plays-the-standards.jpg" alt="Lupin the Third Jazz Plays the &quot;Standards&quot; Fusion CD cover" />
+        <dl class="dl-horizontal">
+          <dt>Company:</dt><dd>Vap</dd>
+          <dt>Catalog #:</dt><dd>VPCG-84785</dd>
+          <dt>Release Date:</dt><dd>2003 May 21</dd>
+          <dt>Price:</dt><dd>2500&yen;</dd>
+          <dt># of Discs:</dt><dd>1</dd>
+          <dt>Artist(s):</dt><dd>Yuji Ohno Trio</dd>
+        </dl>
+      </div>
+    </div>
+
+    <div class="col-md-8">
+      <h2>Lupin the Third Jazz Plays the &quot;Standards&quot;</h2>
+      <ol>
+        <li>C Jam Blues</li>
+        <li>Autumn Leaves</li>
+        <li>Smoke Gets In Your Eyes</li>
+        <li>Softly... As in a Morning Sunrise</li>
+        <li>When You Wish Upon a Star</li>
+        <li>Theme from Lupin the Third</li>
+      </ol>
+    </div>
+  </div>
+
+  <div class="row margin-bottom-30">
+    <div class="col-md-8">
+      <h2>Lupin the Third Jazz Christmas</h2>
+      <ol>
+        <li>Jingle Bells</li>
+        <li>Rudolph the Red-Nosed Reindeer</li>
+        <li>Holy but Easy</li>
+        <li>Santa Claus Is Coming to Town</li>
+        <li>Greensleeves</li>
+        <li>Love Squall</li>
+        <li>White Christmas</li>
+        <li>Candlelight</li>
+        <li>Winter Wonderland</li>
+        <li>The Christmas Song</li>
+      </ol>
+    </div>
+
+    <div class="col-md-4">
+      <div class="rounded-2x service-block-light">
+        <img class="img-responsive center-block" src="/dist/asset/img/music/lupin-the-third-jazz-christmas.jpg" alt="Lupin the Third Jazz Christmas CD cover" />
+        <dl class="dl-horizontal">
+          <dt>Company:</dt><dd>Vap</dd>
+          <dt>Catalog #:</dt><dd>VPCG-84800</dd>
+          <dt>Release Date:</dt><dd>2003 November 21</dd>
+          <dt>Price:</dt><dd>2500&yen;</dd>
+          <dt># of Discs:</dt><dd>1</dd>
+          <dt>Artist(s):</dt><dd>Yuji Ohno Trio &amp; Friends (all tracks)</dd>
+        </dl>
+      </div>
+    </div>
+  </div>
+
 </div>
-<?php
-/*
-/Users/sprak3000/github.com/sprak3000/LupinIIIEncyclopedia/public/dist/asset/img/music/guys-heart-charlies-lupin-songs.jpg
-/Users/sprak3000/github.com/sprak3000/LupinIIIEncyclopedia/public/dist/asset/img/music/isnt-it-more-lupintic.jpg
-/Users/sprak3000/github.com/sprak3000/LupinIIIEncyclopedia/public/dist/asset/img/music/lupin-the-third-jazz-another-jazz.jpg
-/Users/sprak3000/github.com/sprak3000/LupinIIIEncyclopedia/public/dist/asset/img/music/lupin-the-third-jazz-bossa-fusion.jpg
-/Users/sprak3000/github.com/sprak3000/LupinIIIEncyclopedia/public/dist/asset/img/music/lupin-the-third-jazz-christmas.jpg
-/Users/sprak3000/github.com/sprak3000/LupinIIIEncyclopedia/public/dist/asset/img/music/lupin-the-third-jazz-plays-the-standards.jpg
-/Users/sprak3000/github.com/sprak3000/LupinIIIEncyclopedia/public/dist/asset/img/music/lupin-the-third-jazz-the-3rd-funky-pop.jpg
-/Users/sprak3000/github.com/sprak3000/LupinIIIEncyclopedia/public/dist/asset/img/music/lupin-the-third-part-iii-ongakushu.jpg
-/Users/sprak3000/github.com/sprak3000/LupinIIIEncyclopedia/public/dist/asset/img/music/lupin-the-third-rebirth-from-71-original-score.jpg
-  */
-?>
