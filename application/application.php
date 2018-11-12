241c2c5f7e89ce94b2b06d0b0152fb317ee4e878
Luis Cruz <sprak3000+github@gmail.com>
2018-11-11 22:13:45 -0500

    Issue #60: Upgrade to Slim3
    * Require Slim3 via composer
    * Convert to use new \Slim\app constructor. Some code polish / PSR-2 formatting in there.
    * Still much to do to get everything working...

--- application/application.php
+++ application/application.php
@@ -1,23 +1,24 @@
 <?php
-use Slim\Slim;
+
 use sprak3000\lupinencyclopedia\Slim\Page;
 use sprak3000\lupinencyclopedia\Slim\Middleware\KnownRedirects;
 
-$config = array ();
-$config['templates.path'] = '../template';
-$config['view'] = new Page\View();
+$config = [
+    'templates.path' => '../template',
+    'view' => new Page\View(),
+];
 
 /** Create the application */
-$app = new Slim($config);
+$app = new \Slim\app($config);
 
 /** Redirect middleware */
 $app->add(new KnownRedirects());
 
 /** Iterate over the available routes */
-$dirIterator = new RecursiveDirectoryIterator("../route");
+$dirIterator = new RecursiveDirectoryIterator('../route');
 $iterator = new RecursiveIteratorIterator($dirIterator, RecursiveIteratorIterator::SELF_FIRST);
 foreach ($iterator as $file) {
-  if($file->isFile() && 'php' === $file->getExtension()) {
-    require_once $file->getPathname();
-  }
+    if ($file->isFile() && $file->getExtension() === 'php') {
+        require_once $file->getPathname();
+    }
 }
