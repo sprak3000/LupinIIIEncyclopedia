daa4c44fe6240c8f35f5bbae7c1ee39584a9383c
sprak <sprak3000+github@gmail.com>
2020-10-25 14:21:18 -0400

    Issue #66: Expand fifth TV series details
    Add in summary from ANN and details on runtime, etc.

--- template/view/anime/tv-series/fifth.php
+++ template/view/anime/tv-series/fifth.php
@@ -18,15 +18,26 @@
                         <div class="row">
                             <div class="col-md-10">
                                 <p>
-                                    The &quot;Blue Jacket&quot; series continues its run with the location moving to
-                                    France. It will begin airing in
-                                    April 2018 on NTV with streaming avaailable on Hulu and other services.
+                                    Lupin sniffs out a fresh source of income - jacking the digital profits from a pack
+                                    of dark web drug dealers who distribute narcotics online via an illegal marketplace
+                                    called Marco Polo. It might be digital cash this time, but there's still a safe – in
+                                    this case, a fortress-like cloud server in the middle of the ocean – and there's
+                                    still a key, namely Ami, the hacker who built Marco Polo's payment systems. Lupin's
+                                    ready to do what it takes to get her to open up, but she's already about to crack –
+                                    and requests the legendary thief's aid in her escape. Meanwhile, Lupin's foes find
+                                    that the best way to capture the thief is to turn to social media.
+                                </p>
+                                <p>
+                                    Plot summary by <a target="_blank" rel="noreferrer noopener" href="http://www.animenewsnetwork.com/">Anime News Network.</a>
                                 </p>
                                 <dl class="dl-horizontal">
-                                    <dt>Japanese Title:</dt>
-                                    <dd><em>Rupan Sansei</em> (<em>Lupin the Third</em>)</dd>
-                                    <dt>Jacket Color:</dt>
-                                    <dd>Blue</dd>
+                                    <dt>Japanese Title:</dt><dd><em>Rupan Sansei PART5</em> (<em>Lupin the Third: Part 5</em>)</dd>
+                                    <dt>Jacket Color:</dt><dd>Blue</dd>
+                                    <dt>Initial Air Date:</dt><dd>2018 April 04</dd>
+                                    <dt>Final Air Date:</dt><dd>2018 September 18</dd>
+                                    <dt>Timeslot:</dt><dd>19:30 - 20:00, Sundays</dd>
+                                    <dt>Episode Count:</dt><dd>24</dd>
+                                    <dt>Run Time per Episode:</dt><dd>~30 minutes</dd>
                                 </dl>
                             </div>
                             <div class="col-md-2">
