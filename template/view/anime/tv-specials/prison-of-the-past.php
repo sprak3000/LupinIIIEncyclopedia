46b69cf73b93d6302c94f4b50baae509f29f6c4e
Luis Cruz <sprak3000+github@gmail.com>
2020-12-28 14:44:09 -0500

    Issue #72 - Add Prison of the Past special (#88)
    Framework needed to display details.

---
+++ template/view/anime/tv-specials/prison-of-the-past.php
@@ -0,0 +1,59 @@
+<div class="container">
+
+  <div class="row">
+
+    <div class="col-md-12">
+
+      <div class="tab-v1">
+
+        <ul class="nav nav-tabs">
+          <li class="active"><a data-toggle="tab" href="#overview">Overview</a></li>
+          <li><a data-toggle="tab" href="#cast">Cast</a></li>
+          <li><a data-toggle="tab" href="#staff">Staff</a></li>
+        </ul>
+
+        <div class="tab-content">
+
+          <div id="overview" class="tab-pane fade in active">
+            <div class="row">
+              <div class="col-md-10">
+                <dl class="dl-horizontal">
+                  <dt>Japanese Title:</dt><dd><em>Rupan Sansei: Prison of the Past</em></dd>
+                  <dt>Jacket Color:</dt><dd>Black</dd>
+                  <dt>Release Date:</dt><dd>2019 November 29</dd>
+                  <dt>Run Time:</dt><dd>90 minutes</dd>
+                </dl>
+                <div class="headline">
+                  <h4>Synopsis</h4>
+                </div>
+                <p>
+                    Lupin and the gang try to break a master thief out of prison to share in his loot, but they've got
+                    competition, and they'll have to get to him first.
+                </p>
+                <p>
+                    Synopsis from Netflix
+                </p>
+              </div>
+              <div class="col-md-2">
+                <img class="img-responsive" src="/dist/asset/img/anime/tv-specials/prison-of-the-past/prison-of-the-past.jpg" alt="Prison of the Past" />
+              </div>
+            </div>
+          </div>
+
+          <div id="cast" class="tab-pane fade in">
+              <?php echo $app->render('partial/cast.php', ['partial' => true, 'annData' => $annData]); ?>
+          </div>
+
+          <div id="staff" class="tab-pane fade in">
+              <?php echo $app->render('partial/staff.php', ['partial' => true, 'annData' => $annData]); ?>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
