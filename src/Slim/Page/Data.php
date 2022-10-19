6826f9bede04899f7f150023671e376bc1074447
Luis Cruz <sprak3000+github@gmail.com>
2022-10-19 13:03:04 -0400

    Issue #94 - Fix items related to forum upgrade (#95)
    - Adjust forum RSS URL link.
    - Adjust fields used to display data from RSS feed items.

--- src/Slim/Page/Data.php
+++ src/Slim/Page/Data.php
@@ -99,7 +99,7 @@ class Data
         $this->addScrollbar();
 
         try {
-            $this->forumRSS = simplexml_load_file("http://www.lupinencyclopedia.com/forums/index.php?action=.xml");
+            $this->forumRSS = simplexml_load_file("https://www.lupinencyclopedia.com/forums/index.php?action=.xml;type=rss2");
         } catch (Exception $e) {
             // TODO: Log this appropriately
         }
