efbccdf6a23650e0937635e8454540198b6c9107
sprak <sprak3000+github@gmail.com>
2023-02-27 18:58:00 -0500

    Remove Google Analytics.

--- template/layout/header.php
+++ template/layout/header.php
@@ -67,13 +67,6 @@ if( isset( $inlineCss ) && is_array( $inlineCss ) && !empty( $inlineCss ) )
   </head>
 
   <body class="header-fixed">
-    <?php // Google Tag Manager ?>
-    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-TBBR" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-TBBR');</script>
 
 <?php
 if(!$is404) {
